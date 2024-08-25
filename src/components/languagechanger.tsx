1485aef49a220446c288df00e629129c5e5b149e
demiroo <demiroezkan205@gmail.com>
2024-08-26 00:26:40 +0200

    language switcher

---
+++ src/components/languagechanger.tsx
@@ -0,0 +1,41 @@
+'use client'
+import { useRouter, usePathname } from '@/navigation';
+import React from 'react';
+
+export default function LanguageChanger({ locale }) {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const handleTextClick = (lang) => {
+    router.push(pathname, { locale: lang });
+  };
+
+  return (
+    <div className="flex space-x-2 mb-4"> 
+      <div>
+        <span
+        className='bg-black font-medium px-2 py-1 text-xs cursor-pointer text-white rounded-lg hover:bg-slate-300 hover:text-slate-900'
+        onClick={() => handleTextClick('de')}
+        >
+          DE
+        </span>
+      </div>
+      <div>
+        <span
+        className='bg-black font-medium px-2 py-1 text-xs cursor-pointer text-white rounded-lg hover:bg-slate-300 hover:text-slate-900'
+        onClick={() => handleTextClick('tr')}
+        >
+          TR
+        </span>
+      </div>
+      <div>
+        <span
+        className='bg-black font-medium px-2 py-1 text-xs cursor-pointer text-white rounded-lg hover:bg-slate-300 hover:text-slate-900'
+        onClick={() => handleTextClick('en')}
+        >
+          EN
+        </span>
+      </div>
+    </div>
+  );
+}
